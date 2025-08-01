24168ad20aaa12aacd8f0bfeb85e1109907e6895
Vickygreene123 <okereaforfavour867@gmail.com>
2025-08-01 17:51:26 +0100

    fixed ?

--- src/components/popups/PopUp.tsx
+++ src/components/popups/PopUp.tsx
@@ -5,8 +5,8 @@ const PopUpContainer: React.FC<{
   title: string;
   onClose: () => void;
   children: React.ReactNode;
-  // footer?: React.ReactNode;
-}> = ({ title, children, onClose }) => {
+  footer?: React.ReactNode;
+}> = ({ title, children, onClose , footer }) => {
   return (
     <div className="fixed inset-0 bg-[#00000066]  flex items-center justify-center z-[1000] absolute ">
       <div className="bg-white rounded-[16px] max-h-[85vh] overflow-y-auto w-[577px] shadow-lg relative flex flex-col">
@@ -27,7 +27,13 @@ const PopUpContainer: React.FC<{
         <div className="px-6 py-4 pb-16 space-y-4 text-xs">{children}</div>
 
 {/* Footer */}
-				<div className="flex justify-end gap-4 mt-3 p-2 bg-[#F8F9FF] w-full sticky bottom-0 z-10">
+            {
+              footer ?  (
+                <div className="absolute bottom-0 left-0 right-0 border-t border-[#C3C6CF] bg-white p-4">
+                  {footer}
+                </div>
+              ) : (
+                				<div className="flex justify-end gap-4 mt-3 p-2 bg-[#F8F9FF] w-full sticky bottom-0 z-10">
 					<button
 						className="px-3 py-1 text-xs bg-[#D4E3FF] rounded  text-[#0360AB] h-[28px]"
 						onClick={onClose}
@@ -41,6 +47,9 @@ const PopUpContainer: React.FC<{
 						Done
 					</button>
 				</div>
+              )
+            }
+
       </div>
     </div>
   );
